d1e29da691ae46d3872ac224134866488ae90b5a
crumiano <rumiano.c@coditt.fr>
2015-01-30 00:17:49 +0100

    "Refacoring and add units tests"

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    nodeunit: {
+      files: ['test/**/*_test.js'],
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      },
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'nodeunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      },
+    },
+    simplemocha: {
+      options: {
+        globals: ['should'],
+        timeout: 3000,
+        ignoreLeaks: false,
+        //grep: '*-test',
+        ui: 'bdd',
+        reporter: 'list'
+      },
+      all: { src: ['test/**/*.js'] }
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  grunt.registerTask('test', ['simplemocha']);
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
+
+};
---
+++ test/index_test.js
@@ -0,0 +1,86 @@
+var API = require('../lib/'),
+    chai = require('chai'),
+    expect = chai.expect,
+    _ = require('lodash'),
+    config = require('../lib/config.js');
+
+chai.should();
+
+describe('Main', function() {
+    beforeEach(function() {
+        API.default();
+    });
+
+    describe('#config', function() {
+        it('should change the culture', function(done) {
+            config.lang.should.equal("en");
+            API.setCulture("fr");
+            config.lang.should.equal("fr");
+            done();
+        });
+
+        it('should change the forecsast type', function(done) {
+            config.forecastType.should.equal("");
+            API.setForecastType("daily");
+            config.forecastType.should.equal("daily");
+            done();
+        });
+
+        it('should set the APPID', function(done) {
+            config.APPID.should.equal("");
+            API.setAPPID("Best APPID ever");
+            config.APPID.should.equal("Best APPID ever");
+            done();
+        });
+
+        it('should reset default settings', function(done) {
+        	config.lang.should.equal("en");
+            API.setCulture("fr");
+            config.lang.should.equal("fr");
+
+            config.forecastType.should.equal("");
+            API.setForecastType("daily");
+            config.forecastType.should.equal("daily");
+
+            config.APPID.should.equal("");
+            API.setAPPID("Best APPID ever");
+            config.APPID.should.equal("Best APPID ever");
+
+            API.default();
+            config.lang.should.equal("en");
+            config.forecastType.should.equal("");
+            config.APPID.should.equal("");
+
+            done();
+        });
+    });
+
+    describe('#query', function() {
+        it('should get the weather for lyon with name', function(done) {
+            API.now("Lyon", function(err, data) {
+                expect(err).to.not.exist;
+                expect(data).to.exist;
+                expect(data).to.have.keys(['type', 'values']);
+                done();
+            });
+        });
+
+        it('should get 3 hours the weather for lyon with id', function(done) {
+            API.now(2996944, function(err, data) {
+                expect(err).to.not.exist;
+                expect(data).to.exist;
+                expect(data).to.have.keys(['type', 'values']);
+                done();
+            });
+        });
+
+        it('should get 3 hours the weather for lyon with coordinates', function(done) {
+            API.now([45.75,4.85], function(err, data) {
+                expect(err).to.not.exist;
+                expect(data).to.exist;
+                expect(data).to.have.keys(['type', 'values']);
+                done();
+            });
+        });
+    });
+});
